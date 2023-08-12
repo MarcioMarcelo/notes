738a7349a2e62c4b4fa3e481ef62b424cd48eefe
etecmarciomarcelo <etecmarciomarcelo@gmail.com>
2023-08-11 22:02:45 -0300

    Adicionando notas no projeto Note

--- NotePage.xaml.cs
+++ NotePage.xaml.cs
@@ -2,8 +2,28 @@ namespace Note;
 
 public partial class NotePage : ContentPage
 {
+	string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
 	public NotePage()
-	{
+{
+	
 		InitializeComponent();
+
+		if (File.Exists(_fileName))
+	TextEditor.Text = File.ReadAllText(_fileName);
 	}
+
+	private void SaveButton_Clicked(object sender, EventArgs e)
+	{
+		File.WriteAllText(_fileName, TextEditor.Text);
+	}
+
+	private void DeleteButton_Clicked(Object sender, EventArgs e) 
+	{
+		if (File.Exists(_fileName))
+		{ 
+		File.WriteAllText(_fileName, TextEditor.Text);
+        }
+        TextEditor.Text = string.Empty;
+    }
 }
